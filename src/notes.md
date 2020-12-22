ae7100501f761de24982f5b8a2ff719e7c7b9b14
Alexander Goedde <alexander.goedde@gmail.com>
2020-12-22 16:43:48 +0100

    sync

---
+++ src/notes.md
@@ -0,0 +1,12 @@
+Fields:
+
+- AutoField --> default (name: "ID")
+- CharField
+- DurationField
+- DateField
+- Boolean (?)
+- ImageField
+- JSONField --> PostgreSQL
+- TextField
+
+- MarkDown --> for the reviews
--- src/records/models.py
+++ src/records/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Link(models.Model):
+    # wikipedia, discogs, streaming services (Spotify, Tidal, Apple ...)
+    class Type(models.TextChoices):
+        WIKIPEDIA = 'wikipedia', 'Wikipedia'
+        SPOTIFY = 'spotify', 'Spotify'
+        # FIXME!
+
+    label = models.CharField(max_length=200, choices=Type.choices)
+    url = models.URLField()
+
+    class Meta:
+        abstract = True
+    
+
+class Artist(models.Model):
+    name = models.CharField(max_length=500)
+
+
+class ArtistLink(Link):
+    artist = models.ForeignKey(Artist, related_name="links", on_delete=models.CASCADE)
+
+
+class Record(models.Model):
+    artist = models.ManyToManyField(Artist, related_name="records")
+    title = models.CharField(max_length=1000)
+
+
+class RecordLink(Link):
+    record = models.ForeignKey(Record, related_name="links", on_delete=models.CASCADE)
